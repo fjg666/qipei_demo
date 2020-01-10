0a3171bf167938e0110e3acb3cf9c477209e0044
fjg <421507690@qq.com>
2020-01-10 17:14:03 +0800

    duandian

--- LKT/webapp/modules/app/actions/userAction.class.php
+++ LKT/webapp/modules/app/actions/userAction.class.php
@@ -511,7 +511,7 @@ class userAction extends Action {
         $message_type = trim($request->getParameter('message_type')); // 短信类型
         $message_type1 = trim($request->getParameter('message_type1')); // 短信类别
 
-        echo json_encode(array('code'=>202,'message'=>'验证码是：123456'));
+        echo json_encode(array('code'=>219,'message'=>'验证码是：123456'));
         exit();
         //$Tools = new Tools($db, $store_id, 1);
         //$res = $Tools->generate_code($db,$mobile,$message_type,$message_type1);
